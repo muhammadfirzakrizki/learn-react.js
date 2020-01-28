4978e8a3453ac2508f862856cd98ebb898b9642c
firzakrizki@gmail.com <firzakrizki@gmail.com>
2020-01-28 14:08:05 +0700

    StateLess Component & StateFull Component

---
+++ src/container/StateFullComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StateFullComponent extends React.Component {
+    render() {
+        return <p>StateFull Component Class</p>
+    }
+}
+
+export default StateFullComponent;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HelloWorldComponent from './component/HelloComponent';
+import StateFullComponent from './container/StateFullComponent';
 
 const Helloworld = () => {
-    return ( <
-        p > Muhammad Firzak Rizki < /p>
+    return ( 
+        <p> Muhammad Firzak Rizki </p>
     )
 }
 
-class StateFullComponent extends React.Component {
+class StateFull extends React.Component {
     render() {
-        return <p > StateFull Component < /p>
+        return <p> StateFull Component </p>
     }
 }
 
-ReactDOM.render( < StateFullComponent / > , document.getElementById('root'));
-
+ReactDOM.render( < HelloWorldComponent / > , document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
